34d7ce74284a443ed3195488b4dc6dd2f7177cca
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-15 16:19:27 +0200

    command to call other shell script

--- install_all.sh
+++ install_all.sh
@@ -1,7 +1,12 @@
 # install all required library, file, configurations necessary to set up an authentification server
+echo "--- Installing set-up for OpenID Connect server ---"
 
 # Gluu library (Java)
+echo "setting up Gluu library: "
+./install_Gluu.sh
 
 # Keycloak server
+echo "setting up Keycloak server "
+#./install_Keycloak.sh
 
 # authentification app?
