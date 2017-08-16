701d47df86066eda422302fc07ca038b46a8169c
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-16 11:32:20 +0200

    Update install_Gluu_RP.sh

--- install_Gluu_RP.sh
+++ install_Gluu_RP.sh
@@ -1,3 +1,4 @@
 echo "hello"
 echo "deb https://repo.gluu.org/ubuntu/ xenial main" | sudo tee --append /etc/apt/sources.list.d/gluu-repo.list > /dev/null
 curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
+sudo apt-get update
