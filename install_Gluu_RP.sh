c309dfa472b8a7f60e060627beda673f0b7e33b7
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-16 11:31:12 +0200

    this actually works

--- install_Gluu_RP.sh
+++ install_Gluu_RP.sh
@@ -1,3 +1,3 @@
 echo "hello"
 echo "deb https://repo.gluu.org/ubuntu/ xenial main" | sudo tee --append /etc/apt/sources.list.d/gluu-repo.list > /dev/null
-curl https://repo.gluu.org/ubuntu/gluu-apt.key | tac | tac | apt-key add -
+curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
