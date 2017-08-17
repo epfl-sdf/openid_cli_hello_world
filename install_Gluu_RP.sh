c8e9fea8e1d01d50b91ac8f17e45a91ac43cd856
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-17 08:56:11 +0200

    install_all calls all install files

--- install_Gluu_RP.sh
+++ install_Gluu_RP.sh
@@ -3,3 +3,5 @@ echo "deb https://repo.gluu.org/ubuntu/ xenial main" | sudo tee --append /etc/ap
 curl https://repo.gluu.org/ubuntu/gluu-apt.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install gluu-oxd-server
+
+./install_conf_Gluu.sh
