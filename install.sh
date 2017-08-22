2f022ab7408cb6e2a882c26bab456a6575686bcd
Brassens <31003267+Brassens@users.noreply.github.com>
2017-08-22 15:05:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ sudo apt-get install -y python3 python3-pip
 echo ------------ install virtualenv
 sudo pip2 install virtualenv
 #Create virtual environement folder
-virtualenv ./.venv
+sudo virtualenv ./.venv
 
 #Install python 2.7.12
 sudo apt-get install python2.7.12
