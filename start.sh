8b52d200c019c802e00df7f5af8f01075e99110b
Pablo Mercado Del Rio <pablo.mercadodelrio@epfl.chl>
2017-08-28 12:38:33 +0200

    modifie©(essaye de scripter les commandes screen pour le proxy), le start.sh est non fonctionnel

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # petit script pour démarrer le Hello Wolrd du client ipenid
-#zf170821.1529
+#zf170821.1529, 170828.1237
+
+echo "attention ca ne fonctionne pas encore"
+exit
 
 # Teste si les arguments sont bien passés 
 #if (( $# < 2 ))
@@ -24,12 +27,17 @@
 #adresse IP du serveur
 THEIP=$(/sbin/ifconfig ens18 | /bin/grep "inet ad" | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
 
+#Tentative d'implementation du screen
+
+
+
+
 
 #creation de la session screen "proxy"
-screen -S proxy
+#screen -S proxy
 
 #creation de la session screen "client"
-screen -S client
+#screen -S client
 
 echo -e " 
 Afin de garder le proxy WEB permanent, il serait bien de le faire tourner dans un 'screen' avec:
@@ -46,29 +54,38 @@ https://$THEIP:5443
 
 "
 
-echo "Liste des screens actifs"
-screen -list
+#echo "Liste des screens actifs"
+#screen -list
 
-echo '------ Créé le screen "client" --------
-Pour switcher au screen "client" afin le log de l'application client, exécuter: "screen -r client"'
+#echo '------ Créé le screen "client" --------
+#Pour creer une fenêtre "client" afin de voir le log du client, faire le raccourci "ctrl-a" puis "c", puis exécuter: "screen -r client"'
 
-"
-echo '
------- Créé le screen "proxy" --------
-Pour switcher au screen "proxy" afin d'observer les échanges serveur-client, exécuter: "screen -r proxy"
+#"
+#echo '
+#------ Créé le screen "proxy" --------
+#Pour creer une fenêtre "proxy" afin d'observer les interactions serveur-client, faire le raccourci "ctrl-a" puis "c", puis ex  cuter: "screen -r proxy"
 
-'
-echo '
-Pour tabber à travers les screens: "CTRL+A", puis "n"
-Plus d'informations sur le fonctionnement de screen: "https://docs.google.com/document/d/1MqJu9ppWayAEUfN6YZkVTt7ERt509_Ake9oafjKemL4/edit"
+#'
+#echo '
+#Pour tabber à travers les screens: "CTRL+A", puis "n"
+#Plus d'informations sur le fonctionnement de screen: "https://docs.google.com/document/d/1MqJu9ppWayAEUfN6YZkVTt7ERt509_Ake9oafjKemL4/edit"
 
-'
+#'
+
+#lancement du proxy sur la session proxy
+#screen -S proxy -X mitmproxy
 
 #creation du dossier virtuel et lancement du programme
+#screen -S client -X virtFold="venvOpenid"
+#screen -S client -X source $virtFold/bin/activate
+
+#screen -S client -X python app.py
+
+#screen -S client -X deactivate
+
 virtFold="venvOpenid"
 source $virtFold/bin/activate
 
 python app.py
 
 deactivate
-
