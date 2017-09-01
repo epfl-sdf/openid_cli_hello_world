1f2a123ef1369aa05579e981a2a802c3b507f606
Christian Zufferey <christian@zufferey.com>
2017-09-01 15:40:52 +0200

    - corrigé les script start.sh afin de ne plus avoir l'erreur SNI

--- mitmproxy/start.sh
+++ mitmproxy/start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # lancement du proxy a partir de l'environnement virtuel
+#zf170901.1530
+
 virtFold="venvMitmproxy"
 source $virtFold/bin/activate
 
-mitmproxy
+# pour les problèmes de SNI il faut lire
+# http://docs.mitmproxy.org/en/stable/features/passthrough.html
+ 
+#mitmdump -v
+mitmdump --ignore ^10\.92\.104\.167:5443$
+#mitmproxy
+
 
 deactivate
